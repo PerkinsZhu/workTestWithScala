3236d2e503a5e5c046fc241af616adc9322b045e
PerkinsZhu <pingjing_zhu@sina.com>
2017-10-11 12:49:31 +0800

    决策树代码优化一

--- src/main/scala/zpj/machinelearning/DesicionTree.scala
+++ src/main/scala/zpj/machinelearning/DesicionTree.scala
@@ -9,7 +9,7 @@ import scala.collection.mutable.Map
   * Created by PerkinsZhu on 2017/10/9 19:03. 
   */
 object DesicionTree {
-//  计算信息熵 注意：信息熵和特征没有关系，只和最后的结果有关系
+  //  计算信息熵 注意：信息熵和特征没有关系，只和最后的结果有关系
   def getShannonEntropy(dataList: List[List[Any]]): Double = {
     import scala.collection.mutable.Map
     val labelMap: Map[String, Float] = Map()
@@ -26,34 +26,24 @@ object DesicionTree {
 
   def getSplitData(dataList: List[List[Any]], i: Int, value: Any) = {
     //返回的数据为：那些在i位置上的特征值为Value的数据，移除该特征之后的其它特征和结果集。注意理解这里返回的是什么！！！！
-    dataList.filter(item => item(i) == value).map(item => item.dropWhile(ele => ele == value))
+    dataList.filter(item => item(i) == value).map(item => item.filter(ele => ele != value))
   }
 
   /**
     * 计算决策树的下一个节点为什么特征，返回的是该特征在dataList中的index
+    *
     * @param dataList
     * @return
     */
   def getBestFeture(dataList: List[List[Any]]): Int = {
     val baseEntropy = getShannonEntropy(dataList)
-    var tempEntrop = 0.0d
-    var fetureIndex = 0
-    for (i <- 0 until dataList(0).length - 1) {
-      val branch = dataList.map(item => item(i)).toSet
-      var itemEntrop = 0.0d
-      for (value <- branch) {
-        //计算各个分支的信息熵
+    val temp = for (i <- 0 until dataList(0).length - 1) yield {
+      (baseEntropy - dataList.map(item => item(i)).distinct.map(value =>{
         val tempDate = getSplitData(dataList, i, value)
-        val prob = (tempDate.length toDouble) / dataList.length.toDouble
-        itemEntrop = itemEntrop + prob * getShannonEntropy(tempDate)
-      }
-      itemEntrop = baseEntropy - itemEntrop
-      if (itemEntrop > tempEntrop) {
-        fetureIndex = i;
-        tempEntrop = itemEntrop
-      }
+        ((tempDate.length toDouble) / dataList.length.toDouble) * getShannonEntropy(tempDate)
+      }).sum )
     }
-    fetureIndex
+    temp.indexOf(temp.max)
   }
 
   def createDesicionTree(dataList: List[List[Any]], labelList: List[Any]): String = {
@@ -71,18 +61,15 @@ object DesicionTree {
   def doHandel(dataList: List[List[Any]], labelList: List[Any], dealLabel: Option[String]): JsObject = {
     //FIXME 看是否能够使用尾递归进行优化
     val tempValue = dataList.map(item => item.reverse.head)
-    if (tempValue.toSet.size == 1) {
-      //没有分支了
+    if (tempValue.toSet.size == 1) {//没有分支了
       return Json.obj(dealLabel.get -> tempValue(0).toString)
     }
-    if (dataList(0).length == 1) {
-      //没有属性了
+    if (dataList(0).length == 1) {//没有属性了
       return Json.obj(dealLabel.get -> majorityCnt(tempValue))
     }
 
     val bestFteureIndex = getBestFeture(dataList)
     val label = labelList(bestFteureIndex)
-
 
     val newLabels = labelList.filter(item => item != label)
     var treeNode: JsObject = Json.obj()
@@ -90,9 +77,9 @@ object DesicionTree {
     temp.foreach(item => {
       val splitData = getSplitData(dataList, bestFteureIndex, item)
       val tempNode = doHandel(splitData, newLabels, Some(item.toString));
-      treeNode ++= (if (tempNode.keys.head == item) {
+      treeNode ++= (if (tempNode.keys.head == item) {//返回的最后结果
         Json.obj(item.toString -> tempNode.values.head)
-      } else {
+      } else {//返回的为下一个节点
         Json.obj(item.toString -> tempNode.value)
       })
     })
@@ -100,14 +87,10 @@ object DesicionTree {
   }
 
   def main(args: Array[String]): Unit = {
-
     val dataSet = List(List("会呼吸", "会飞", "YES"), List("会呼吸", "不会飞", "YES"), List("不会呼吸", "会飞", "YES"), List("不会呼吸", "不会飞", "NO"))
     val labelSet = List("会呼吸吗？", "会飞吗？")
-
-    //    print(getShannonEntropy(dataSet))
-    //    print(getBestFeture(dataSet))
     val data = MLTool.readFileToList("E:\\zhupingjing\\sources\\git\\workTestWithScala\\src\\main\\scala\\zpj\\machinelearning\\testData\\data01.txt")
-    //    println(createDesicionTree(data.init, data.last))
-    println(createDesicionTree(dataSet, labelSet))
+    println(createDesicionTree(data.init, data.last))
+    // println(createDesicionTree(dataSet, labelSet))
   }
 }
