e6d8d58447b9d6a85ad74cecdf27d3ceb054c0dd
PerkinsZhu <perkinszhu@sina.cn>
2018-04-08 10:07:34 +0800

    master cherr1

--- src/main/scala/zpj/baseTest/BaseTest.scala
+++ src/main/scala/zpj/baseTest/BaseTest.scala
@@ -726,7 +726,7 @@ def isRight(data:String,statue:Boolean): Boolean= {
 
 
   }
-
+//master  test1
   import play.api.libs.json._
   import play.api.libs.json.Reads._
   import play.api.libs.functional.syntax._
