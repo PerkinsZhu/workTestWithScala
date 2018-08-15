acb497c57fe08a9ffad41e67acbc46911ad8630c
Perkins Zhu <perkinszhu@sina.cn>
2018-08-15 17:49:08 +0800

    测试WSClient默认超时时间

--- src/main/scala/zpj/tools/WSClientTool.scala
+++ src/main/scala/zpj/tools/WSClientTool.scala
@@ -5,6 +5,8 @@ import akka.stream.ActorMaterializer
 import org.scalatest.{FlatSpec, FunSuite}
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 /**
   * Created by PerkinsZhu on 2018/3/19 12:44
@@ -16,12 +18,33 @@ object WSClientTool {
   private implicit val materializer = ActorMaterializer()
   val wsClient = StandaloneAhcWSClient()
 
+  def testTimeOut(): Unit = {
+    //play WSClient 默认超时时间: 120000 ms
+//    java.util.concurrent.TimeoutException: Request timeout to /192.168.10.192:8094 after 120000 ms
+
+    val start = System.currentTimeMillis()
+    wsClient.url("http://192.168.10.192:8094/test/timeOut").get().map(respond => {
+      println(respond.body)
+    }).onComplete {
+      case Success(value) => {
+        println("success ->" + (System.currentTimeMillis() - start))
+        println(value)
+      }
+      case Failure(exception) => {
+        println("failure ->" + (System.currentTimeMillis() - start))
+        exception.printStackTrace()
+      }
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    /*    wsClient.url("http://www.baidu.com").get().map(respond => {
+    /*    wsClient.url("http://192.168.10.192:8094/test/timeOut").get().map(respond => {
+
           println(respond.body)
         }).onComplete {
           case _ => close()
         }*/
+    testTimeOut()
   }
 
   def close(): Unit = {
