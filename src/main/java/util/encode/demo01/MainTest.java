f22201076f8e01e7e5f035a3e9d003e3b66261ca
Perkins Zhu <perkinszhu@sina.cn>
2018-09-27 16:24:13 +0800

    test RSA、AES、Base64

---
+++ src/main/java/util/encode/demo01/MainTest.java
@@ -0,0 +1,38 @@
+package util.encode.demo01;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class MainTest {
+
+    public static void main(String[] args) throws Exception {
+        String filepath = System.getProperty("user.dir")+ File.separator+"temp\\encode";
+
+
+        System.out.println("--------------公钥加密私钥解密过程-------------------");
+        //        String plainText="ihep_公钥加密私钥解密";
+        String json = "{" + "\"bgyprogressuuid\": \"aabc891fc62f41728497da2904294e16\"," + "\"bgysupplier\": \"供应商名称3\"," + " \"bgyitemname\": \"项目公司名称3\"," + "\"bgyitemcode\": \"0003\"," + "\"bgyarea\": \"佛山区域3\"," + "\"bgycontractname\": \"合同名称\"," + "\"bgycontractno\": \"合同编号003\"," + "\"bgypayamount\": 888.989," + "\"bgysupplierbank\": \"供应商开户行：中国银行\"," + "\"bgysupplieraccount\": \"供应商账号：88888989989\"," + "\"bgysupplieraccountname\": \"收方户名：佛山碧桂园\"," + "\"bgypaystatus\": false," + "\"dr\": 0}" + "{" + "\"bgyprogressuuid\": \"aabc891fc62f41728497da2904294e15\"," + "\"bgysupplier\": \"供应商名称3\"," + " \"bgyitemname\": \"项目公司名称3\"," + "\"bgyitemcode\": \"0003\"," + "\"bgyarea\": \"佛山区域3\"," + "\"bgycontractname\": \"合同名称\"," + "\"bgycontractno\": \"合同编号003\"," + "\"bgypayamount\": 888.989," + "\"bgysupplierbank\": \"供应商开户行：中国银行\"," + "\"bgysupplieraccount\": \"供应商账号：88888989989\"," + "\"bgysupplieraccountname\": \"收方户名：佛山碧桂园\"," + "\"bgypaystatus\": false," + "\"dr\": 0}";
+        //公钥加密过程
+        byte[] cipherData = RSAEncrypt.encrypt(RSAEncrypt.loadPublicKeyByStr(RSAEncrypt.loadPublicKeyByFile(filepath)), json.getBytes());
+        String cipher = Base64.encode(cipherData);
+        //私钥解密过程
+        byte[] res = RSAEncrypt.decrypt(RSAEncrypt.loadPrivateKeyByStr(RSAEncrypt.loadPrivateKeyByFile(filepath)), Base64.decode(cipher));
+        String restr = new String(res);
+        System.out.println("原文：" + json);
+        System.out.println("加密：" + cipher);
+        System.out.println("解密：" + restr);
+        System.out.println();
+
+    }
+
+    @Test
+    public void testCreateRAS() {
+        //创建随机的公钥和私钥
+        String path = System.getProperty("user.dir")+ File.separator+"temp\\encode";
+        System.out.println(path);
+        RSAEncrypt.genKeyPair(path);
+    }
+
+
+}
---
+++ src/main/java/util/encode/demo03/Base64Test.java
@@ -0,0 +1,85 @@
+package util.encode.demo03;
+
+import org.junit.Test;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+/**
+ * Created by PerkinsZhu on 2018/9/27 11:52
+ **/
+public class Base64Test {
+
+    public String getEncodedBase64(String plainText) {
+        String encoded = null;
+        try {
+            byte[] bytes = plainText.getBytes("UTF-8");
+            encoded = Base64.getEncoder().encodeToString(bytes);
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+        return encoded;
+    }
+
+    public byte[] getDecodedBase64(String plainText) {
+        byte[] decoded = null;
+        try {
+            byte[] bytes = plainText.getBytes("UTF-8");
+            decoded = Base64.getDecoder().decode(bytes);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return decoded;
+    }
+
+    @Test
+    public void testBase64() throws UnsupportedEncodingException {
+        String content = "hello=";
+        String data = getEncodedBase64(content);
+        System.out.println("加密结果：" + data);
+        byte[] result = getDecodedBase64(data);
+        System.out.println("解密结果：" + new String(result, "UTF-8"));
+    }
+
+    String src = "hello ";
+
+    @Test
+    // 用jdk实现
+    public void jdkBase64() {
+        try {
+            BASE64Encoder encoder = new BASE64Encoder();
+            String encode = encoder.encode(src.getBytes());
+            System.out.println("encode:" + encode);
+            BASE64Decoder decoder = new BASE64Decoder();
+            System.out.println("decode:" + new String(decoder.decodeBuffer(encode)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    // 用Apache的common codes实现
+    public  void commonsCodesBase64() {
+        byte[] encodeBytes = org.apache.commons.codec.binary.Base64.encodeBase64(src.getBytes());
+        System.out.println("common codes encode:" + new String(encodeBytes));
+        byte[] dencodeBytes = org.apache.commons.codec.binary.Base64.decodeBase64(encodeBytes);
+        System.out.println("common codes decode:" + new String(dencodeBytes));
+    }
+
+/*    @Test
+    // 用bouncy castle实现
+    public  void bouncyCastleBase64(){
+        byte[] encodeBytes = org.bouncycastle.util.encoders.Base64.encode(src.getBytes());
+        System.out.println("bouncy castle encode:" + new String(encodeBytes));
+        byte[] dencodeBytes = org.bouncycastle.util.encoders.Base64.decode(encodeBytes);
+        System.out.println("bouncy castle decode:" + new String(dencodeBytes));
+    }*/
+
+}
