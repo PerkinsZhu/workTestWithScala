bf3e75ed81b8bcc2f68ce99c9deed4b70c27911a
Perkins Zhu <perkinszhu@sina.cn>
2018-12-17 20:21:47 +0800

    akkaTimer

--- src/main/scala/zpj/akka/actor/ActorTimer.scala
+++ src/main/scala/zpj/akka/actor/ActorTimer.scala
@@ -1,20 +1,50 @@
 package zpj.akka.actor
 
-import akka.actor.{Actor, Timers}
-
 /**
   * Created by PerkinsZhu on 2017/12/6 17:03. 
   */
-class ActorTimer {
 
+import akka.actor.{Actor, ActorSystem, Props, Timers}
+import akka.util.Timeout
 
-}
-package zz{
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+object MyTimerActor {
 
-private class MyActor extends Actor with Timers{
-  override def receive: Receive = {
-  case "1"=>println("----")
+  private case object TickKey
+
+  private case object FirstTick
+
+  private case object Tick
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("testSystem")
+    val actor = system.actorOf(Props[MyTimerActor])
+    actor ! MyTimerActor.FirstTick
+    implicit val timeout = Timeout(5 seconds)
+    import akka.pattern.{ ask, pipe }
+    val future = actor ? MyTimerActor.FirstTick
+    future.map(println(_))
+
+    Thread.sleep(Int.MaxValue)
+
+  }
 }
-  timers
-}
+
+class MyTimerActor extends Actor with Timers {
+
+  import MyTimerActor._
+
+  timers.startSingleTimer(TickKey, FirstTick, 500.millis)
+
+  def receive = {
+    case FirstTick ⇒
+      println("receive first tick")
+      timers.startPeriodicTimer(TickKey, Tick, 1.second)
+      sender() !"12313"
+    case Tick ⇒
+      println("receive tick")
+  }
+
 }
