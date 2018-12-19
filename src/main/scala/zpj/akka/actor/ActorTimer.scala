597832dadae1f7c3e58259357d39f140268851f8
Perkins Zhu <perkinszhu@sina.cn>
2018-12-19 10:45:34 +0800

    stop actor(stop\kill\PoisonPill\gracefulStop)

--- src/main/scala/zpj/akka/actor/ActorTimer.scala
+++ src/main/scala/zpj/akka/actor/ActorTimer.scala
@@ -4,12 +4,14 @@ package zpj.akka.actor
   * Created by PerkinsZhu on 2017/12/6 17:03. 
   */
 
-import akka.actor.{Actor, ActorSystem, Props, Timers}
+import akka.Done
+import akka.actor.{Actor, ActorSystem, CoordinatedShutdown, Kill, PoisonPill, Props, Timers}
 import akka.util.Timeout
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
+
 object MyTimerActor {
 
   private case object TickKey
@@ -23,11 +25,29 @@ object MyTimerActor {
     val actor = system.actorOf(Props[MyTimerActor])
     actor ! MyTimerActor.FirstTick
     implicit val timeout = Timeout(5 seconds)
-    import akka.pattern.{ ask, pipe }
+    import akka.pattern.{ask, pipe}
     val future = actor ? MyTimerActor.FirstTick
     future.map(println(_))
 
-    Thread.sleep(Int.MaxValue)
+    import akka.pattern.gracefulStop
+
+    val stopped01: Future[Boolean] = gracefulStop(actor, 5 seconds, "stop message or PoisonPill")
+
+    val stopped: Future[Boolean] = gracefulStop(actor, 2 seconds)
+    stopped.foreach(status => println("actor停止状态：" + status))
+
+    actor ! PoisonPill.getInstance
+    actor ! PoisonPill
+
+    actor ! Kill
+
+    //添加阶段任务,在执行CoordinatedShutdown.******事件执行前指向该任务
+    CoordinatedShutdown(system).addTask(CoordinatedShutdown.PhaseActorSystemTerminate, "system shutDown ") { () ⇒ {println("--- i am  shut down "); Future.successful(Done)} }
+
+
+    system.terminate()
+    //    Thread.sleep(Int.MaxValue)
+
 
   }
 }
@@ -38,13 +58,18 @@ class MyTimerActor extends Actor with Timers {
 
   timers.startSingleTimer(TickKey, FirstTick, 500.millis)
 
+  override def postStop {
+    println("Number5::postStop called")
+  }
+
   def receive = {
     case FirstTick ⇒
       println("receive first tick")
       timers.startPeriodicTimer(TickKey, Tick, 1.second)
-      sender() !"12313"
+      sender() ! "12313"
     case Tick ⇒
       println("receive tick")
+    case "stop" => context stop self
   }
 
 }
